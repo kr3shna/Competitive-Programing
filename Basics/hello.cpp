7cacf7644fb4494b989850dd8d67b21c09cbf738
kr3shna <krishna2shende@gmail.com>
2024-04-23 00:53:29 +0530

    Hello

---
+++ Basics/hello.cpp
@@ -0,0 +1,10 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    string name;
+    cin >> name;
+    cout <<"Hello " << name;
+}
---
+++ Basics/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    str name;
+    cin >> name;
+    cout <<"Hello" << name;
+}
