682e74ca3ede615c5a43de98b68feb52f4b9da06
kr3shna <krishna2shende@gmail.com>
2024-04-23 01:56:45 +0530

    Difference

---
+++ Basics/difference.cpp
@@ -0,0 +1,12 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
+
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    long long x,a,b,c,d;
+    cin >>a >>b >>c >>d;
+    x = (a*b)-(c*d);
+    cout << "Difference = " <<x;
+}
