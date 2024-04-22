0400f6bd4b0f783b22e842e0ac6105ca3e3b320b
kr3shna <krishna2shende@gmail.com>
2024-04-23 01:44:54 +0530

    Simple Calculator

---
+++ Basics/simple_calculator.cpp
@@ -0,0 +1,12 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/C
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    long long x,y;
+    cin >>x >>y;
+    cout <<x <<" + " <<y <<" = "  <<x+y <<endl;
+    cout <<x <<" * " <<y <<" = "  <<x*y <<endl;
+    cout <<x <<" - " <<y <<" = "  <<x-y;
+}
