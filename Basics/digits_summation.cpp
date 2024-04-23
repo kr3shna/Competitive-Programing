f9614d462dc02396e86c1c5e4a65ebc03c0d2826
kr3shna <krishna2shende@gmail.com>
2024-04-23 08:58:37 +0530

    Digit Summation

---
+++ Basics/digits_summation.cpp
@@ -0,0 +1,9 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/F
+
+#include<iostream>
+using namespace std;
+int main(){
+    long long a,b;
+    cin >>a >>b;
+    cout <<(a%10) + (b%10) <<"\n";
+}
