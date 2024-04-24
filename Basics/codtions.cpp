c07408f65cbbbc5aefba5684df78ec00bbdcd156
kr3shna <krishna2shende@gmail.com>
2024-04-24 10:13:36 +0530

    Welcome for you with conditions

---
+++ Basics/codtions.cpp
@@ -0,0 +1,10 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin >>a >>b;
+    (a >= b) ? cout <<"YES" : cout <<"NO";
+}
