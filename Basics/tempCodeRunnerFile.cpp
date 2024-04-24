4ebd56de3a1c330f608e55ab805fbe935bd8c670
kr3shna <krishna2shende@gmail.com>
2024-04-24 11:49:09 +0530

    The Brothers

--- Basics/tempCodeRunnerFile.cpp
+++ Basics/tempCodeRunnerFile.cpp
@@ -1,10 +1,34 @@
+
 #include <iostream>
 using namespace std;
 int main(){
-    long long n,summation;
-    cin >>n;
-    for(int i=1; i<=n; i++){
-        summation += i;
-        cout << summation;
+    int a,b,c;
+    cin >>a >>b >>c;
+    if(a > b && a < c){
+        cout <<a <<endl;
+        if(b < c){
+            cout <<b;
+        }
+        else{
+            cout <<c;
+        }
+    }
+    if(b > a && b < c){
+        cout <<b <<endl;
+        if(a < c){
+            cout <<a;
+        }
+        else{
+            cout <<c;
+        }
+    }
+    if(c > b && c < a){
+        cout <<c <<endl;
+        if(b < a){
+            cout <<b;
+        }
+        else{
+            cout <<a;
+        }
     }
 }
