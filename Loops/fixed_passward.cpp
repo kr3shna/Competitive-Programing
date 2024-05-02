886fe4aa2109f46275746454b3740a2f9600beb6
kr3shna <krishna2shende@gmail.com>
2024-05-03 00:02:35 +0530

    Table Multiplication

---
+++ Loops/fixed_passward.cpp
@@ -0,0 +1,10 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/D
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cin >>x;
+    (x == 1999) ? cout <<"Correct" : cout <<"Wrong";
+}
