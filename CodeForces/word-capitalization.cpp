cd2495d55835bb24f20c024586ede2b512336244
kr3shna <krishna2shende@gmail.com>
2024-05-24 23:58:54 +0530

    Word capitalization

---
+++ CodeForces/word-capitalization.cpp
@@ -0,0 +1,15 @@
+// https://codeforces.com/problemset/problem/281/A
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   string word;
+    cin >> word;
+    word[0] = toupper(word[0]);
+    cout << word << endl;
+
+    return 0;
+}
