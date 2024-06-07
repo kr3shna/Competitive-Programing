ce5bb19a1f211eca79fd119721e24c9849250ff6
kr3shna <krishna2shende@gmail.com>
2024-06-07 23:39:21 +0530

    Expression

---
+++ CodeForces/expression.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/problemset/problem/479/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+ 
+    int result1 = a + b + c;
+    int result2 = a * b * c;
+    int result3 = (a + b) * c;
+    int result4 = a * (b + c);
+    int result5 = a + (b * c);
+    int result6 = (a * b) + c;
+    int max_value = max({result1, result2, result3, result4, result5, result6});
+    cout <<max_value <<endl;
+ 
+    return 0;
+}
