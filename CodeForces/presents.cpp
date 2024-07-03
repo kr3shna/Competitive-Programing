bbd89a42b3b8285c4bba9f8d02f15f7a63251158
kr3shna <krishna2shende@gmail.com>
2024-07-03 23:50:24 +0530

    Presents

---
+++ CodeForces/presents.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    vector<int> p(n + 1); 
+    vector<int> result(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> p[i];
+    }
+    for (int i = 1; i <= n; i++) {
+        result[p[i]] = i;
+    }
+    for (int i = 1; i <= n; i++) {
+        cout << result[i] << " ";
+    }
+}
