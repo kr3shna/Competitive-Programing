f5ede4d8cfcb644418cd8118be5085910b82edc5
kr3shna <krishna2shende@gmail.com>
2024-07-05 18:06:20 +0530

    Summation from 1 to N

---
+++ cpp/datatypes_and_conditions/summation_from_1_to_n.cpp
@@ -0,0 +1,11 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/G
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    cin >>n;
+    cout << n * (n +1) / 2;
+}
