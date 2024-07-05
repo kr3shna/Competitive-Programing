ec91c5b8cf17e57f6be47ced893b6a9b9b4b14a7
kr3shna <krishna2shende@gmail.com>
2024-07-05 17:58:11 +0530

    Digits Summation

---
+++ cpp/datatypes_and_conditions/digits_summation.cpp
@@ -0,0 +1,12 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/F
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long x,y,m,n;
+    cin >>x >>y;
+    m = x % 10;
+    n = y % 10;
+    cout << m + n;
+}
