44e1679f66d964f7dfb1b62f462a6b626712617f
kr3shna <krishna2shende@gmail.com>
2024-07-05 17:39:22 +0530

    Difference

---
+++ cpp/datatypes_and_conditions/difference.cpp
@@ -0,0 +1,10 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long a,b,c,d;
+    cin >>a >>b >>c >>d;
+    cout <<"Difference = " <<(a*b) - (c*d);
+}
