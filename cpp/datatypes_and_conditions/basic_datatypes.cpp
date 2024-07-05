3a8d2c661b04c5a89ed143f065be96ee759f40d5
kr3shna <krishna2shende@gmail.com>
2024-07-05 17:19:58 +0530

    Basic Data Types

---
+++ cpp/datatypes_and_conditions/basic_datatypes.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/B
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int i;
+    long long l;
+    char ch;
+    float f;
+    double d;
+    cin >>i >>l >>ch >>f >>d;
+    cout <<i <<endl <<l <<endl <<ch <<endl <<f <<endl <<d;
+}
