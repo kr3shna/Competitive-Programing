aa18378310c888cac55b35a2e7d6506b80d825d1
kr3shna <krishna2shende@gmail.com>
2024-07-05 17:49:21 +0530

    Area of a Circle

---
+++ cpp/datatypes_and_conditions/area_of_circle.cpp
@@ -0,0 +1,11 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/E
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    double pi = 3.141592653;
+    double r;
+    cin >>r;
+    cout  <<fixed <<setprecision(9) <<pi * r * r;
+}
