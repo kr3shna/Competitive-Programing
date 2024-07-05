48e46174f1990663c0af6bface58e3a246df6ecc
kr3shna <krishna2shende@gmail.com>
2024-07-05 23:42:55 +0530

    Calculator

---
+++ cpp/datatypes_and_conditions/calculator.cpp
@@ -0,0 +1,25 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/O
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    char s;
+    cin >>a >>s >>b;
+    if(s == '+'){
+        cout <<a+b;
+    }
+    if(s == '-'){
+        cout <<a-b;
+    }
+    if(s == '*'){
+        cout <<a*b;
+    }
+    if(s == '/'){
+        cout <<a/b;
+    }
+    
+}
+    
+    
