9ffe64a33f4907138160fbf44664933657810479
kr3shna <krishna2shende@gmail.com>
2024-07-07 21:01:48 +0530

    Sum of Squares

---
+++ cpp/loops/sum_of_squares.cpp
@@ -0,0 +1,10 @@
+// https://codeforces.com/group/jtU6D2hVEi/contest/104934/problem/A
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >>n;
+    cout <<(n*(n+1)*(2*n+1)) / 6;
+}
