6797fd6d94f10f8880d20b3c6e256eb8a1c9b988
kr3shna <krishna2shende@gmail.com>
2024-08-31 16:22:21 +0530

    Hello

---
+++ InJava/datatypes_conditionals/hello.java
@@ -0,0 +1,10 @@
+package InJava.datatypes_conditionals;
+
+import java.util.*;
+public class hello {
+    public static void main(String[] arags){
+        Scanner Scan = new Scanner(System.in);
+        String name = Scan.nextLine();
+        System.out.println("Hello, " + name);
+    }
+}
