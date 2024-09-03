a2e48fa67af8bbce332ce3b51d64c4ebc7dc628e
kr3shna <krishna2shende@gmail.com>
2024-09-03 23:52:33 +0530

    Fixed Passward

---
+++ cpp/loops/fixedPassword.cpp
@@ -0,0 +1,16 @@
+https://codeforces.com/group/MWSDmqGsZm/contest/219432/my
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int password;
+    while (cin >> password) {
+        if (password == 1999) {
+            cout<< "Correct" <<endl;
+            return 0;
+        } else {
+            cout<< "Wrong" <<endl;
+        }
+    }
+
+    return 0;
+}
